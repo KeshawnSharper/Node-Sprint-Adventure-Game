a000697cbb77e973a01d57c601da8552d8d28550
keshawn0998 <keshawn@contenda.co>
2021-12-30 12:53:19 -0500

    added

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs')
+app.get('/:name', function(req, res) {
+res.render('pages/index',{name:req.params.name});
+});
+app.get('/Location/:location', function(req, res) {
+res.render('pages/location',{location:req.params.location});
+});
+app.get('/Area/:area', function(req, res) {
+    res.render('pages/area',{area:req.params.area});
+    });
+  app.listen(8080);
+console.log('Server is listening on port 8080');
